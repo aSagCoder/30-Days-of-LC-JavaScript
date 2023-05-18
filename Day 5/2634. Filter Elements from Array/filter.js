e76a05e275b9d101ab8c466a11d277d7fd59ad79
Kuhu Bhatnagar <89660722+aSagCoder@users.noreply.github.com>
2023-05-19 00:02:32 +0530

    Create filter.js

---
+++ Day 5/2634. Filter Elements from Array/filter.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    var filteredArray = [];
+
+    for (var i = 0; i < arr.length; i++){
+        if (fn(arr[i], i)) {
+            filteredArray.push(arr[i]);
+        }
+    }
+
+    return filteredArray;
+};

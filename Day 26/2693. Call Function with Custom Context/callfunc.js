373de91e93889ac970ef133e9fe2c430df1a739c
Kuhu Bhatnagar <89660722+aSagCoder@users.noreply.github.com>
2023-06-05 20:35:41 +0530

    Create callfunc.js

---
+++ Day 26/2693. Call Function with Custom Context/callfunc.js
@@ -0,0 +1,8 @@
+Function.prototype.callPolyfill = function(context, ...args) {
+  Object.defineProperty(context, 'fn', {
+    value: this,
+    enumerable: false,
+  });
+
+  return context.fn(...args);
+}
